73f02ee59de3247862084defb3c8a883b8952ad4
Tom Denley <tsdenley@gmail.com>
2017-04-15 16:20:52 +0100

    First commit; Basic React/Webpack setup

---
+++ readme.md
@@ -0,0 +1,13 @@
+# React JS ES2015 Basic Setup (with NPM, Babel & webpack)
+
+## Install Dependencies
+
+`npm install`
+
+## Run in Dev Mode & Watch
+
+`npm run dev`
+
+## Build for Production
+
+`npm run build`
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack'),
+	path = require('path'),
+
+	BUILD_DIR = path.resolve(__dirname, 'src/client/public'),
+	APP_DIR = path.resolve(__dirname, 'src/client/app')
+
+module.exports = config = {
+	entry: APP_DIR + '/index.jsx',
+	output: {
+		path: BUILD_DIR,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?/,
+				include: APP_DIR,
+				loader: 'babel-loader'
+			}
+		]
+	}
+}
